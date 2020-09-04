b2e780cddbe1e22f66906e9fbe6712d0d7f50d87
CommanderBrian <40133130+BrianPineau@users.noreply.github.com>
2020-09-04 14:03:37 -0500

    Scrollmagic set up

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-let header = document.querySelector('header');
+let header = document.querySelector('.header-initial');
+let innerHeader = document.querySelector('.header-container');
+
+//  Init ScrollMagic
+$(document).ready()
+let controller = new ScrollMagic.Controller();
+
+//  Build a scene
+let scene = new ScrollMagic.Scene({
+  triggerElement: 'header'
+})
+  .setClassToggle('header', 'shrinkHeader') // Add class to header
+  .addTo(controller);
+
+
+
+
+
+
+// window.addEventListener('scroll', function() {
+//     if (window.pageYOffset > 10) {
+//         headerCollapse.classList.add('pop-out');
+//         moveIcons.classList.add('icon-move');
+//     } else {
+//         headerCollapse.classList = 'header';
+//         moveIcons.classList = ' icon-set';
+//     }
+// }); 
+
+
+
 
