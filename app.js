f131745610d2bf5558176a05f7054fd5697533a9
CommanderBrian <40133130+BrianPineau@users.noreply.github.com>
2020-09-08 21:56:29 -0500

    header styling and skills container

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 let entireHeader = document.querySelector('.header-initial');
-let innerHeader = document.querySelector('.header-container');
 
 //  Init ScrollMagic
 // $(document).ready()
@@ -11,26 +10,14 @@ let scene = new ScrollMagic.Scene({
   triggerHook: '0'
 })
   .setClassToggle(entireHeader, 'header-shrink') // Add class to header
-  scene.addIndicators({
-    name: 'shrink header', // custom name for your scene
-    colorStart: 'red', // custom color - colorEnd
-    colorEnd: 'blue',
-    colorTrigger: 'green',
-   })
+  // scene.addIndicators({
+  //   name: 'shrink header', // custom name for your scene
+  //   colorStart: 'red',
+  //   colorTrigger: 'green',
+  //  })
   .addTo(controller);
 
 
-
-
-// window.addEventListener('scroll', function() {
-//     if (window.pageYOffset > 10) {
-//         headerCollapse.classList.add('pop-out');
-//         moveIcons.classList.add('icon-move');
-//     } else {
-//         headerCollapse.classList = 'header';
-//         moveIcons.classList = ' icon-set';
-//     }
-// }); 
 
 
 
