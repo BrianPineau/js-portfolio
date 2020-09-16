94ec2058f8ce6d7d80c68b5c22ab889ad15cb08c
CommanderBrian <40133130+BrianPineau@users.noreply.github.com>
2020-09-15 19:59:25 -0500

    Contact section framework set up

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 let entireHeader = document.querySelector('.header-initial');
 
 
-//Scroll Effects
-//  Init ScrollMagic
-let controller = new ScrollMagic.Controller();
 
-//  Build a scene
-let scene = new ScrollMagic.Scene({
-  triggerElement: '.hook-lead',
-  triggerHook: '0'
-})
-  .setClassToggle(entireHeader, 'header-shrink') // Add class to header
-  // scene.addIndicators({
-  //   name: 'shrink header', // custom name for your scene
-  //   colorStart: 'red',
-  //   colorTrigger: 'green',
-  //  })
-  .addTo(controller);
+//Website Loader
+document.onreadystatechange = function() { 
+  if (document.readyState !== "complete") { 
+      document.querySelector("body").style.visibility = "hidden"; 
+      document.querySelector("#loader").style.visibility = "visible"; 
+  } else { 
+      document.querySelector("#loader").style.display = "none"; 
+      document.querySelector("body").style.visibility = "visible"; 
+  } 
+}; 
 
 
 
@@ -36,4 +31,27 @@ typewriter.typeString('Make websites great again!')
     .typeString('<strong>altered!</strong>')
     .pauseFor(2500)
     .start();
+
+
+
+//Scroll Effects
+//  Init ScrollMagic
+let controller = new ScrollMagic.Controller();
+
+//  Build a scene
+let scene = new ScrollMagic.Scene({
+  triggerElement: '.hook-lead',
+  triggerHook: '0'
+})
+  .setClassToggle(entireHeader, 'header-shrink') // Add class to header
+  // scene.addIndicators({
+  //   name: 'shrink header', // custom name for your scene
+  //   colorStart: 'red',
+  //   colorTrigger: 'green',
+  //  })
+  .addTo(controller);
+
+
+
+
 
